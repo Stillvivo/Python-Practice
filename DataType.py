cf92399472c56c535ad7e890cc8fc0e26db87b5d
Alirezalt <aillotfe.0919@gmail.com>
2026-07-04 02:36:32 +0330

    Add files via upload

---
+++ DataType.py
@@ -0,0 +1,45 @@
+list = ["alireza","mohammad","ghasem","hossein","matin","farhad","asghar","mohsen","pouria"]
+
+
+# print(list[:len(list)//2]) # half of We're list.
+
+# print(list) # Print List.
+
+# print(list[-1]) # Print Last One Index.
+
+# print(len(list)) # Print Length.
+
+# print(list[0:3:2]) # Print 0 Index To 3 Index and step is 2.
+
+# list[0] = "Mojtaba" # Replace Index 0 To "Mojtaba".
+
+# list.append(40) # For Add Index In last One of List.
+
+# list.insert(0,40) # For Add Index In EveryWhere We Need to Add.
+
+# list.remove(list[0]) # Remove Any Index or Choice Index.
+
+# list.pop() # Remove Index.
+
+# del list[0] # Remove Index.
+
+# list.sort(reverse=True) # For Sort List (Reverse).
+
+# list.reverse() # For Reverse List.
+
+# print(list.count("alireza")) # For Find Index Object.
+
+# print(list.index("ghasem")) # For Find Index Object.
+
+# c = list.copy() # For Copy Of List.
+
+# Matrix = [
+#     [1,2,3],
+#     [4,5,6],
+#     [7,8,9]
+# ]
+# print(Matrix[0][2]) # For Create Matrix.
+
+
+
+
