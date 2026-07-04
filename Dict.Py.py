31ecf205025e91b4f02e0a89e24548ecec6ce192
alireza lotfi <aillotfe.0919@gmail.com>
2026-07-04 03:40:16 +0330

    .

--- Dict.Py.py
+++ Dict.Py.py
@@ -1,6 +1,51 @@
 Person = {
-    "Name" : "ALireza",
+    "Name" : "Alireza",
     "Age" : 29,
     "City": "Tehran",
     "Gender" : "Male"
-}
+} # For create Dict.
+
+# print(Person) # Show Dict
+
+# print(Person["Name"]) # For Print Dict Quantity.
+
+# print(Person.get("Name")) # For Print Dict Quantity.
+
+# Person["Family"] = "Lotfi" # For Add Quantity.
+
+# Person["Age"] = 30 # For Rebuild Quantity.
+
+# Person.pop("Gender") # For Remove Quantity.
+
+# del Person["Gender"] # For Remove Quantity.
+
+# Person.clear() # Remove All Data From Dict.
+
+# for key in Person:
+#     print(key) # For Print Key.
+
+# for value in Person.values():
+#     print(value) # For Print value.
+
+# for key, value in Person.items():
+#     print(key, value) #  For Print Key And Value.
+
+# Person.keys()
+# Person.values()
+# Person.items() # Important Methods.
+
+# print(len(Person)) # For Length Dict.
+
+# Person.copy() # Copy Of Dict.
+
+
+
+
+
+
+
+
+
+
+
+
